99ba411a43c616100632a4b4f52f58ed865aeff3
Fabiana1vo <fabianasouzaivo@icloud.com>
2025-02-02 11:40:40 -0300

    add: remove thrid el from list

---
+++ removeThird.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class removeThird {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+
+        list.add("Red");
+        list.add("Green");
+        list.add("Orange");
+        list.remove(2);
+
+
+
+
+        System.out.println(list);
+
+    }
+}
